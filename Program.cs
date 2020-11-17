7ab60599f60f80de968e9797d2ea8b69fd2a869a
Joshua Belden <github@joshuabelden.com>
2020-11-16 16:00:01 -0800

    Added Sales By Match solution

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace hackerrank
+{
+    class Program
+    {
+        static void Main()
+        {
+            var result = Solution.sockMerchant(3, new int[] { 2, 2, 3, 3, 4 });
+            Console.WriteLine($"Result: {result}");
+        }
+    }
+}
---
+++ Warmup/SalesByMatch.cs
@@ -0,0 +1,39 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+namespace hackerrank
+{
+    public partial class Solution
+    {
+        public static int sockMerchant(int numberOfSocks, int[] allSocks)
+        {
+            // Create a hash to store socks by color.
+            var coloredSocks = new Dictionary<int, int>();
+
+            foreach (var sockColor in allSocks)
+            {
+                if (coloredSocks.ContainsKey(sockColor))
+                    coloredSocks[sockColor]++;
+                else
+                    coloredSocks.Add(sockColor, 1);
+            }
+
+            var totalPairs = 0;
+            foreach (var sock in coloredSocks)
+                totalPairs += sock.Value / 2; // relying on C#'s implicit integer handling to ignore the remainder.
+
+            return totalPairs;
+        }
+    }
+}
